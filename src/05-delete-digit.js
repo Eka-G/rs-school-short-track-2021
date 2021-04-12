fdb8f5694d624531ad6808e59b6400da8673d673
Eka-G <ekaglazkovaa@yandex.ru>
2021-04-12 15:57:25 +0300

    feat: implement 05 delete diget

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,20 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  let res = '';
+  const num = String(n).split('');
+
+  for (let i = 0; i < num.length - 1; i++) {
+    if (+num[i] < +num[i + 1]) {
+      res += num.slice(i + 1).join('');
+      break;
+    } else {
+      res += num[i];
+    }
+  }
+
+  return +res;
 }
 
 module.exports = deleteDigit;
