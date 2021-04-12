030e9f7967437df67057b4c974fe4d16007cfab5
Eka-G <ekaglazkovaa@yandex.ru>
2021-04-12 11:59:16 +0300

    feat: implement 02 common character count

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -9,8 +9,22 @@
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new Error('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+  if (s1 === '' || s2 === '') return 0;
+
+  const copyS1 = s1.split('').sort().join('');
+  let lastIndex = 0;
+  let res = 0;
+
+  s2.split('').sort().forEach((item) => {
+    const matchIndex = copyS1.indexOf(item, lastIndex);
+    if (matchIndex !== -1) {
+      lastIndex = matchIndex + 1;
+      res++;
+    }
+  });
+
+  return res;
 }
 
 module.exports = getCommonCharacterCount;
