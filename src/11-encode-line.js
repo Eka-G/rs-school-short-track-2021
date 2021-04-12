40e8230f017a7ff8176965a10af726e1716a5b3e
Eka-G <ekaglazkovaa@yandex.ru>
2021-04-12 18:56:39 +0300

    feat: implement 11 encode line

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,26 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let res = '';
+  let count = 1;
+
+  str.split('').forEach((item, i, arr) => {
+    if (item === arr[i + 1]) {
+      count++;
+    }
+
+    if (item !== arr[i + 1]) {
+      if (count > 1) {
+        res += count + item;
+      } else {
+        res += item;
+      }
+      count = 1;
+    }
+  });
+
+  return res;
 }
 
 module.exports = encodeLine;
