37a9c11a966f95e11031da765f3dfc97654f2ace
Eka-G <ekaglazkovaa@yandex.ru>
2021-04-12 20:31:42 +0300

    feat: implement 12 dns starts

--- src/12-dns-stats.js
+++ src/12-dns-stats.js
@@ -20,8 +20,30 @@
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new Error('Not implemented');
+function getDNSStats(domains) {
+  const res = {};
+  const valueCounter = [];
+
+  domains.forEach((item) => {
+    const arr = item.split('.').reverse();
+    arr.forEach((part) => valueCounter.push(part));
+  });
+
+  domains.forEach((item, index) => {
+    const parts = item.split('.').reverse();
+    let key = '';
+
+    if (index === 0) {
+      const startKey = `.${parts[0]}`;
+      res[startKey] = valueCounter.filter((j) => j === parts[parts.length - 1]).length;
+    }
+
+    parts.forEach((x) => { key += `.${x}`; });
+
+    res[key] = valueCounter.filter((j) => j === parts[parts.length - 1]).length;
+  });
+
+  return res;
 }
 
 module.exports = getDNSStats;
