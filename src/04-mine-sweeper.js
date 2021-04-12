44d25cda7158180d9aa7603cbf91b3cc182591c2
Eka-G <ekaglazkovaa@yandex.ru>
2021-04-12 15:23:58 +0300

    feat: implement 04 mine sweeper

--- src/04-mine-sweeper.js
+++ src/04-mine-sweeper.js
@@ -21,8 +21,38 @@
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new Error('Not implemented');
+function check(num, length) {
+  let res = num;
+  if (num < 0) res = 0;
+  if (num >= length) res = (length - 1);
+  return res;
+}
+
+function minesweeper(matrix) {
+  // make empty array same size with matrix
+  const res = new Array(matrix.length)
+    .fill(null)
+    .map(() => new Array(matrix[0].length));
+
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      const maxTop = check((i - 1), matrix.length);
+      const maxBottom = check((i + 1), matrix.length);
+      const maxLeft = check((j - 1), matrix[i].length);
+      const maxRight = check((j + 1), matrix[i].length);
+      let count = 0;
+
+      for (let f = maxTop; f <= maxBottom; f++) {
+        for (let g = maxLeft; g <= maxRight; g++) {
+          if (matrix[f][g]) count++;
+        }
+      }
+
+      res[i][j] = matrix[i][j] ? (count - 1) : count;
+    }
+  }
+
+  return res;
 }
 
 module.exports = minesweeper;
